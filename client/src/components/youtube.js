cd613e9b7d0591494a76222ae081643bdb92c274
Bora Sumer <solasguitarra@hotmail.com>
2019-08-11 21:28:41 -0400

    yarn lock deleted

--- client/src/components/youtube.js
+++ client/src/components/youtube.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-const KEY = 'AIzaSyAFUNYmE1gfydRFrlb3Q05gXlPSgQmiY6I';
+//!This is not being used at the moment
+// import axios from 'axios';
+// const KEY = 'AIzaSyAFUNYmE1gfydRFrlb3Q05gXlPSgQmiY6I';
 
-export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3/',
-  params: {
-    part: 'snippet',
-    maxResults: 10,
-    key: KEY
-  }
-})
+// export default axios.create({
+//   baseURL: 'https://www.googleapis.com/youtube/v3/',
+//   params: {
+//     part: 'snippet',
+//     maxResults: 10,
+//     key: KEY
+//   }
+// })
 
-console.log('hi')
